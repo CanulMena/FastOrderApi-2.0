85396842535c16cbc054f80fd2439bd63d394252
Gustavo Canul <gusssy12@gmail.com>
2024-11-13 00:35:45 -0600

    Agregar las entidades en un archivo de barril.
    se agrupan todas las exportaciones de las entidades para evitar multiples exportaciones en el futuro.

---
+++ src/domain/entities/index.ts
@@ -0,0 +1,7 @@
+export * from "./client.entity";
+export * from "./dish.entity";
+export * from "./kitchen.entity";
+export * from "./side.entity";
+export * from "./order.entity";
+export * from "./order-detail.entity";
+
