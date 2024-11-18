bbf0333437727a4a631df1e4b3c5d26187d41eaa
Gustavo Canul <gusssy12@gmail.com>
2024-11-17 23:14:44 -0600

    create create-kitchen.dto and inplement in postKitchen controller.
    create-kitchen is created to be used in kitchen-controller

---
+++ src/domain/dtos/create-kitchen.dto.ts
@@ -0,0 +1,20 @@
+
+export class CreateKitchenDto {
+  constructor(
+    public readonly name: string, //?Readonly significa que no se puede modificar después de la creación
+    public readonly address: string,
+    public readonly phone: string,
+    public readonly creationDate?: Date | null,
+  ){}
+
+  static create( props: {[key: string]: any} ): [ string?, CreateKitchenDto? ] {
+    const { name, address, phone } = props;
+
+    if ( !name ) return ['Name is required', undefined];
+    if ( !address ) return ['Address is required', undefined]; //todo: Validar que la dirección sea válida en el futuro
+    if ( !phone ) return ['Phone is required', undefined];
+
+    return [undefined, new CreateKitchenDto(name, address, phone)];
+  }
+
+}
--- src/presentation/kitchen/kitchen-controller.ts
+++ src/presentation/kitchen/kitchen-controller.ts
@@ -1,17 +1,18 @@
 import { Request, Response } from 'express';
 import { Kitchen } from '../../domain/entities/index';
+import { CreateKitchenDto } from '../../domain/dtos/index';
 
 const kitchenList: Kitchen[] = [
-    new Kitchen(1, 'Cocina 1', 'Dirección 1', new Date(), '123456789'),
-    new Kitchen(2, 'Cocina 2', 'Dirección 2', new Date(), '123456789'),
-    new Kitchen(3, 'Cocina 3', 'Dirección 3', new Date(), '123456789'),
-    new Kitchen(4, 'Cocina 4', 'Dirección 4', new Date(), '123456789'),
-    new Kitchen(5, 'Cocina 5', 'Dirección 5', new Date(), '123456789'),
-    new Kitchen(6, 'Cocina 6', 'Dirección 6', new Date(), '123456789'),
-    new Kitchen(7, 'Cocina 7', 'Dirección 7', new Date(), '123456789'),
-    new Kitchen(8, 'Cocina 8', 'Dirección 8', new Date(), '123456789'),
-    new Kitchen(9, 'Cocina 9', 'Dirección 9', new Date(), '123456789'),
-    new Kitchen(10, 'Cocina 10', 'Dirección 10', new Date(), '123456789'),
+    new Kitchen(1, 'Cocina 1', 'Dirección 1','123456789'),
+    new Kitchen(2, 'Cocina 2', 'Dirección 2','123456789'),
+    new Kitchen(3, 'Cocina 3', 'Dirección 3','123456789'),
+    new Kitchen(4, 'Cocina 4', 'Dirección 4','123456789'),
+    new Kitchen(5, 'Cocina 5', 'Dirección 5','123456789'),
+    new Kitchen(6, 'Cocina 6', 'Dirección 6','123456789'),
+    new Kitchen(7, 'Cocina 7', 'Dirección 7','123456789'),
+    new Kitchen(8, 'Cocina 8', 'Dirección 8','123456789'),
+    new Kitchen(9, 'Cocina 9', 'Dirección 9','123456789'),
+    new Kitchen(10, 'Cocina 10', 'Dirección 10', '123456789'),
 ];
 
 export class KitchenController {
@@ -41,23 +42,15 @@ export class KitchenController {
     }
 
     public postKitchen = (req: Request, res: Response) => { //POST /api/kitchen
-        const { name, address, phone } = req.body;
-        //TODO: Validar el body por medio del DTO
 
-        const kitchen = new Kitchen(
-            kitchenList.length + 1, //Este es un ejemplo --> la base de datos se encargará de asignar el id
-            name,
-            address,
-            new Date(),
-            phone
-        );
+        const [error, kitchen] = CreateKitchenDto.create(req.body);
 
-        if (kitchen instanceof Kitchen) {
-            kitchenList.push(kitchen); //? push() agrega un elemento al final del array
-            res.status(201).json(kitchen); //? 201: Recurso creado
-        } else {
-            res.status(400).json({error: 'Invalid Kitchen' });
+        if (error) {
+            res.status(400).json({error});
+            return;
         }
+
+        //TODO: Agregar el kitchen al datasource para que se cree en la base de datos
     }
 
     public deleteKitchen = (req: Request, res: Response) => { //DELETE /api/kitchen/:id
