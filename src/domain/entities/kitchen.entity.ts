9571317d95a7b7b596cb7c8de8cc463a263a0bd0
Gustavo Canul <gusssy12@gmail.com>
2024-11-18 14:52:02 -0600

    create fromJson from Kitchen entity
    fromJson is create to receive an object that will be converted to a ktichen entity

--- src/domain/entities/kitchen.entity.ts
+++ src/domain/entities/kitchen.entity.ts
@@ -1,3 +1,4 @@
+
 export class Kitchen {
     constructor(
         public kitchenId: number, //Identificador único de la cocina
@@ -5,5 +6,16 @@ export class Kitchen {
         public address: string, //Dirección para ubicar a la cocina
         public phone: string, //Numero para contactar a la la cocina
     ) {}
-    //TODO: Create from object to create a new instance of the class
+    
+    static fromJson = ( object: {[key: string] : any} ) : Kitchen => {
+        const { id, nombre, direccion, telefono } = object;
+
+        if ( !id ) throw new Error('id is required');
+        if ( !nombre ) throw new Error('nombre is required');
+        if ( !direccion ) throw new Error('direccion is required');
+        if ( !telefono ) throw new Error('telefono is required');
+
+        return new Kitchen(id, nombre, direccion, telefono);
+    }
+
 }
