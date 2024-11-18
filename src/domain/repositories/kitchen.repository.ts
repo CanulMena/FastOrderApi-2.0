893c9dac896831add77671125f3c45f7084c1601
Gustavo Canul <gusssy12@gmail.com>
2024-11-17 23:16:30 -0600

    create kitchen repository

---
+++ src/domain/repositories/kitchen.repository.ts
@@ -0,0 +1,10 @@
+import { CreateKitchenDto } from '../dtos';
+import { Kitchen } from '../entities/index';
+
+export abstract class KitchenRepository {
+  abstract createKitchen( kitchen : CreateKitchenDto ) : Promise<Kitchen>;
+  abstract getKitchens() : Promise<Kitchen[]>;
+  abstract getKitchenById( kitchenId: number ) : Promise<Kitchen>;
+  abstract deleteKitchen( kitchenId: number ) : Promise<Kitchen>;
+  abstract updateKitchen( kitchenId: number, kitchen: CreateKitchenDto ) : Promise<Kitchen>;
+}
