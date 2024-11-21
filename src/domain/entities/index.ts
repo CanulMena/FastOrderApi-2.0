21748a76cb26df1c5543f7cabf6f5773e7b05711
Gustavo Canul <gusssy12@gmail.com>
2024-11-20 22:02:51 -0600

    add user entity to entities index

--- src/domain/entities/index.ts
+++ src/domain/entities/index.ts
@@ -1,6 +1,7 @@
 export * from "./client.entity";
 export * from "./dish.entity";
 export * from "./kitchen.entity";
+export * from "./user.entity";
 export * from "./side.entity";
 export * from "./order.entity";
 export * from "./order-detail.entity";
