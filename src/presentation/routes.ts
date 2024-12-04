0f284a18069836bbffc948ca210503ee0a8488a2
Gustavo Canul <gusssy12@gmail.com>
2024-12-04 01:20:59 -0600

    add user routes in all the routes

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { KitchenRoutes } from "./kitchen/kitchen-routes";
+import { UserRoutes } from "./user/user-routes";
 
 export class AppRoutes {
     static get routes(): Router {
@@ -7,6 +8,8 @@ export class AppRoutes {
         const router = Router();
 
         router.use('/api/kitchen', KitchenRoutes.routes); //Ruta de las cocinas
+
+        router.use('/api/user', UserRoutes.routes); //Ruta de los usuarios
 
         return router;
     }
