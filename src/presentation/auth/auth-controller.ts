f6f690ff0986465c66487e2ce7898a288b017c78
Gustavo Canul <gusssy12@gmail.com>
2024-12-06 18:29:46 -0600

    refactor register user with a dto and create handle error method

--- src/presentation/auth/auth-controller.ts
+++ src/presentation/auth/auth-controller.ts
@@ -1,44 +1,39 @@
 import { Request, Response } from 'express';
 import { UserRepository } from '../../domain/repositories';
 import { CreateUser } from '../../domain/use-case/user/create-user';
+import { RegisterUserDto } from '../../domain/dtos/auth';
+import { CustomError } from '../../domain/errors';
 
 export class AuthController {
   constructor(
     public userRepositoryImpl: UserRepository
   ){}
 
+  private handleError(error: unknown, res: Response) {
+    if (error instanceof CustomError) {
+      return res.status(error.statusCode).json({ error: error.message });
+    }
+    console.log(`${error}`);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+
   public registerUser = async (req: Request, res: Response) => {
-    const { email, passwordHash, rol, kitchenId } = req.body;
-
-    if(!email) {
-      res.status(400).json({ error: 'email is required' });
-      return;
+    const { name, email, password, rol, kitchenId } = req.body;
+    const [ error, registerUserDto ] = RegisterUserDto.create({ name, email, password, rol, kitchenId });
+    if( error ) {
+      res.status(400).json({ error: error });
+      return
     }
-
-    if(!passwordHash) {
-      res.status(400).json({ error: 'password is required' });
-      return;
-    }
-
-    if(!rol) {
-      res.status(400).json({ error: 'rol is required' });
-      return;
-    }
-
-    if (rol !== 'ADMIN' && rol !== 'OPERATOR' && rol !== 'DELIVERY' && rol !== 'SUPER_ADMIN') {
-      res.status(400).json({ error: 'rol is not a valid value' });
-      return;
-    }
-
-    if(kitchenId && typeof kitchenId !== 'number') {
-      res.status(400).json({ error: 'kitchenId is not a number' });
-      return;
-    }
-
     new CreateUser( this.userRepositoryImpl )
-    .exucute({ email, passwordHash, rol, kitchenId })
-    .then( user => res.status(201).json(user) )
-    .catch( error => res.status(404).json({ error: error.message }) ); // 404 Not Found
+    .exucute(registerUserDto)
+    .then( user => {
+      const { passwordHash, ...userEntity } = user;
+      res.status(201).json({ 
+        user: userEntity,
+        token: 'token'
+      });
+    })
+    .catch( error => this.handleError(error, res));
   }
 
   public loginUser = async (req: Request, res: Response) => {
