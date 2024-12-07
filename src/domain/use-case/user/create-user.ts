89bdb3c94cf6dd5f0532acabb39af0a42a0a6411
Gustavo Canul <gusssy12@gmail.com>
2024-12-06 21:13:27 -0600

    add comment

--- src/domain/use-case/user/create-user.ts
+++ src/domain/use-case/user/create-user.ts
@@ -3,7 +3,7 @@ import { RegisterUserDto } from "../../dtos/auth/index";
 import { User } from "../../entities";
 import { UserRepository } from "../../repositories";
 
-//
+//Dentro de los casos de uso, se debe manejar la lógica de negocio de la aplicación
 
 interface CreateUserUseCase {
   exucute(registerUserDto: RegisterUserDto): Promise<User>
