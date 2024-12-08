9e4ec457d1ee4ac9e33955401a8df5ad522aa2eb
Gustavo Canul <gusssy12@gmail.com>
2024-12-07 18:22:20 -0600

    create login user use case

---
+++ src/domain/use-case/user/login-user.ts
@@ -0,0 +1,36 @@
+import { bcryptAdapter } from "../../../configuration/plugins";
+import { LoginUserDto, RegisterUserDto } from "../../dtos/auth";
+import { User } from "../../entities";
+import { CustomError } from "../../errors";
+import { UserRepository } from "../../repositories";
+
+export interface LoginUserUseCase {
+  execute(loginUserDto: LoginUserDto): Promise<object>
+}
+
+export class LoginUser implements LoginUserUseCase {
+
+  constructor(
+    private readonly userRepository: UserRepository
+  ) {}
+
+  async execute(loginUserDto: LoginUserDto): Promise<object> {
+    const userFound = await this.userRepository.getUserByEmail(loginUserDto.email);
+    const isMatchingPassword = await bcryptAdapter.compare(loginUserDto.password, userFound.passwordHash);
+    if (!isMatchingPassword) throw CustomError.badRequest('Invalid password');
+    const { passwordHash, ...userEntity } = User.fromJson({
+      "id": userFound.userId,
+      "nombre": userFound.name,
+      "email": userFound.email,
+      "emailValid": userFound.emailVerified,
+      "contrasena": userFound.passwordHash,
+      "rol": userFound.rol,
+      "cocinaId": userFound.kitchenId
+    });
+
+    return {
+      user: userEntity,
+      token: 'token'
+    };
+  }
+}
