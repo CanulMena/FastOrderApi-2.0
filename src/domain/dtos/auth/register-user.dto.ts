c967231c3b2ae653853f4b04f1411f7eab3daf19
Gustavo Canul <gusssy12@gmail.com>
2024-12-17 11:48:04 -0600

    add validations in registerUserDto

--- src/domain/dtos/auth/register-user.dto.ts
+++ src/domain/dtos/auth/register-user.dto.ts
@@ -1,4 +1,5 @@
 import { regularExps } from "../../../configuration";
+import { User, UserRole } from "../../entities";
 
 export class RegisterUserDto {
 
@@ -6,22 +7,25 @@ export class RegisterUserDto {
     public readonly name: string,
     public readonly email: string,
     public readonly password: string,
-    public readonly rol: 'ADMIN' | 'OPERATOR' | 'DELIVERY' | 'SUPER_ADMIN',
-    public readonly kitchenId: number
+    public readonly rol: UserRole,
+    public readonly kitchenId: number | null,
   ){}
 
-  static create( props: {[key: string]: any} ): [ string?, RegisterUserDto? ] {
+  //Valores falsy de js: undefined, null, 0, NaN, "", o false
+
+  static create( props: {[key: string]: any} ): [ string?, RegisterUserDto? ] { //*uso de tuplas para retornar un error o un objeto
     const { name, email, password, rol, kitchenId } = props;
-
-    if ( !name ) return ['Missing Name'];
-    if ( !email ) return ['Missing Email']; //this is like ['Missing name', undefined]
-    if ( !regularExps.email.test(email) ) return ['Invalid Email'];
-    if ( !password ) return ['Missing Password'];
-    if ( password.length < 6 ) return ['Password must be at least 6 characters long'];
-    if ( rol !== 'ADMIN' && rol !== 'OPERATOR' && rol !== 'DELIVERY' && rol !== 'SUPER_ADMIN' ) return ['Invalid Rol'];
-    // if ( !kitchenId ) return ['Missing Kitchen Id'];
-
+    if (!name) return ['Missing Name'];
+    if (!email) return ['Missing Email'];
+    if (!regularExps.email.test(email) ) return ['Invalid Email'];
+    if (!password) return ['Missing Password'];
+    if (password.length < 6) return ['Password must be at least 6 characters long'];
+    if (!User.isValidRole(rol)) return ['Invalid Role'];
+    if (rol === 'SUPER_ADMIN' && kitchenId) return ['SUPER_ADMIN cannot have a Kitchen ID'];
+    if ((rol === 'ADMIN' || rol === 'OPERATOR' || rol === 'DELIVERY') && !kitchenId ) return ['Missing Kitchen ID'];
+    if (kitchenId){//si kitchenId es distinto de null, undefined, 0, NaN, "", o false
+      if (typeof kitchenId !== 'number') return ['Kitchen ID must be a number'];
+    }
     return [undefined, new RegisterUserDto( name, email, password, rol, kitchenId )];
   }
-
 }
