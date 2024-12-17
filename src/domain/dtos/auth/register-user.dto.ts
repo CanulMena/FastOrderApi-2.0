958dc8ec3b2f68eebcbb75d9e924b27bb538e465
Gustavo Canul <gusssy12@gmail.com>
2024-12-17 14:12:58 -0600

    improve the validatión of the RegisterUserDto
    now to create a RegisterUserDto Kitchen ID must be a positive number or null

--- src/domain/dtos/auth/register-user.dto.ts
+++ src/domain/dtos/auth/register-user.dto.ts
@@ -13,7 +13,7 @@ export class RegisterUserDto {
 
   //Valores falsy de js: undefined, null, 0, NaN, "", o false
 
-  static create( props: {[key: string]: any} ): [ string?, RegisterUserDto? ] { //*uso de tuplas para retornar un error o un objeto
+  static create( props: {[key: string]: any} ): [ string?, RegisterUserDto? ] { //uso de tuplas para retornar un error o un objeto
     const { name, email, password, rol, kitchenId } = props;
     if (!name) return ['Missing Name'];
     if (!email) return ['Missing Email'];
@@ -21,11 +21,9 @@ export class RegisterUserDto {
     if (!password) return ['Missing Password'];
     if (password.length < 6) return ['Password must be at least 6 characters long'];
     if (!User.isValidRole(rol)) return ['Invalid Role'];
+    if (kitchenId !== null && (typeof kitchenId !== 'number' || kitchenId <= 0)) return ['Kitchen ID must be a positive number or null'];
     if (rol === 'SUPER_ADMIN' && kitchenId) return ['SUPER_ADMIN cannot have a Kitchen ID'];
-    if ((rol === 'ADMIN' || rol === 'OPERATOR' || rol === 'DELIVERY') && !kitchenId ) return ['Missing Kitchen ID'];
-    if (kitchenId){//si kitchenId es distinto de null, undefined, 0, NaN, "", o false
-      if (typeof kitchenId !== 'number') return ['Kitchen ID must be a number'];
-    }
+    if ((rol === 'ADMIN' || rol === 'OPERATOR' || rol === 'DELIVERY') && !kitchenId ) return [`${rol} must have a Kitchen ID`];
     return [undefined, new RegisterUserDto( name, email, password, rol, kitchenId )];
   }
 }
