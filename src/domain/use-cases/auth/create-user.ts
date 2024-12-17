e1cb24e4eeb80131108a12f84cc5c25588556ba6
Gustavo Canul <gusssy12@gmail.com>
2024-12-17 14:18:17 -0600

    add kitchen repository en create user use case and use it when registerUserDto.kitchenId is not null

--- src/domain/use-cases/auth/create-user.ts
+++ src/domain/use-cases/auth/create-user.ts
@@ -1,8 +1,8 @@
 import { bcryptAdapter, jwtAdapter } from "../../../configuration/plugins";
 import { RegisterUserDto } from "../../dtos/auth/index";
 import { CustomError } from "../../errors";
-import { UserRepository } from "../../repositories";
-import { SendEmailValidationLink } from './send-e-validation-link';
+import { UserRepository, KitchenRepository } from "../../repositories";
+import { SendEmailValidationLink } from './index';
 
 //Dentro de los casos de uso, se debe manejar la lógica de negocio de la aplicación
 
@@ -13,11 +13,16 @@ interface CreateUserUseCase {
 export class CreateUser implements CreateUserUseCase {
 
   constructor(
+    private readonly kitchenRepository: KitchenRepository,
     private readonly userRepository: UserRepository,
     private readonly sendEmailValidationLink: SendEmailValidationLink
   ) {}
 
   async exucute(registerUserDto: RegisterUserDto): Promise<object> {
+
+    //TODO: Validar que solo un SuperAdmin pueda crear un SuperAdmin o Admin
+
+    if(registerUserDto.kitchenId) await this.kitchenRepository.getKitchenById(registerUserDto.kitchenId);
     const hashedPassword = bcryptAdapter.hash(registerUserDto.password);
     const newRegisterUserDto = { ...registerUserDto, password: hashedPassword };
     const user = await this.userRepository.createUser(newRegisterUserDto);
