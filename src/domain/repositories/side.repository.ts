5065d88f2328b19ef305624282721eed66b56116
Gustavo Canul <gusssy12@gmail.com>
2024-12-25 04:34:14 -0600

    refactor side repository

--- src/domain/repositories/side.repository.ts
+++ src/domain/repositories/side.repository.ts
@@ -5,6 +5,6 @@ export abstract class SideRepository {
     abstract createSide( side : CreateSideDto ) : Promise<Side>;
     abstract getSides() : Promise<Side[]>;
     abstract getSideById( sideId: number ) : Promise<Side>;
-    abstract deleteSide( sideId: number ) : Promise<Side>;
+    abstract deleteBySide( sideId: number ) : Promise<Side>;
     abstract updateSide( side: UpdateSideDto ) : Promise<Side>;
 }
