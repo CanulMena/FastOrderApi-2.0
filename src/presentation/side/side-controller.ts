73fa941678500cb09f7f13fb4697c849d551cb6b
Gustavo Canul <gusssy12@gmail.com>
2024-12-25 04:43:01 -0600

    implement side use cases in the contoller

--- src/presentation/side/side-controller.ts
+++ src/presentation/side/side-controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { SideRepository } from "../../domain/repositories/index";
 import { CreateSideDto, UpdateSideDto } from '../../domain/dtos/side/index';
+import { CreateSide, DeleteSide, GetSide, GetSides, UpdateSide } from '../../domain/use-cases/side';
+import { CustomError } from '../../domain/errors';
 
 export class SideController {
 
@@ -8,22 +10,32 @@ export class SideController {
         public sideRepository: SideRepository
     ) {}
 
+    private handleError(error: unknown, res: Response) {
+        if (error instanceof CustomError) {
+        return res.status(error.statusCode).json({ error: error.message });
+        }
+        console.log(`${error}`);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+
     public getSides = (req: Request, res: Response) => {
-        this.sideRepository.getSides()
+        new GetSides(this.sideRepository)
+        .execute()
         .then( sides => res.status(200).json(sides))
-        .catch( error => res.status(500).json(error));
+        .catch( error => this.handleError(error, res));
     }
 
     public getSideById = (req: Request, res: Response) => {
-        const sideId = +req.params.kitchenId;
+        const sideId = +req.params.sideId;
 
         if ( isNaN(sideId) ) {
             res.status(400).json({error: 'ID argument is not a number'});
         }
 
-        this.sideRepository.getSideById(sideId)
+        new GetSide(this.sideRepository)
+        .execute(sideId)
         .then(side => res.status(200).json(side))
-        .catch(error => res.status(404).json({error: error.message}));
+        .catch( error => this.handleError(error, res));
     }
 
     public postSide = (req: Request, res: Response) => {
@@ -35,35 +47,38 @@ export class SideController {
             return;
         }
 
-        this.sideRepository.createSide(sideDto!)
+        new CreateSide(this.sideRepository)
+        .execute(sideDto!)
         .then( side => res.status(201).json(side))
-        .catch( error => res.status(404).json({error: error.message}));
+        .catch( error => this.handleError(error, res));
     }
 
     public deleteSide = (req: Request, res: Response) => {
-        const sideId = +req.params.kitchenId;
+        const sideId = +req.params.sideId;
 
         if ( isNaN(sideId) ) {
             res.status(400).json({error: 'ID argument is not a number'});
         }
 
-        this.sideRepository.deleteSide(sideId)
+        new DeleteSide(this.sideRepository)
+        .execute(sideId)
         .then( side => res.status(200).json(side))
-        .catch( error => res.status(404).json({error: error.message}));
+        .catch( error => this.handleError(error, res));
     }
 
     public updateSide = ( req: Request, res: Response ) => {
-        const kitchenId = +req.params.kitchenId;
-        const [error, updateSideDto] = UpdateSideDto.create({...req.body, kitchenId});
+        const sideId = +req.params.sideId;
+        const [error, updateSideDto] = UpdateSideDto.create({...req.body, sideId});
 
         if ( error ) {
             res.status(400).json({error});
             return;
         }
 
-        this.sideRepository.updateSide(updateSideDto!)
+        new UpdateSide(this.sideRepository)
+        .execute(updateSideDto!)
         .then( side => res.status(200).json(side))
-        .catch( error => res.status(404).json({error: error.message}));
+        .catch( error => this.handleError(error, res));
     }
 
 }
