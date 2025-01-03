3803d8ddd278ebace3ceeaab9476c1ef1ddf11b1
Gustavo Canul <gusssy12@gmail.com>
2025-01-03 15:18:56 -0600

    implement GetSide use case in DishController constructor to add it in createDish

--- src/presentation/dish/dish-controller.ts
+++ src/presentation/dish/dish-controller.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from 'express';
 import { DishRepository } from '../../domain/repositories';
-import { CreateDishDto } from '../../domain/dtos/dish/create-dish.dto';
+import { CreateDishDto } from '../../domain/dtos/dish/index';
 import { CustomError } from '../../domain/errors';
 import { CreateDish } from '../../domain/use-cases/dish/index';
+import { GetSide } from '../../domain/use-cases/side';
+import { User } from '../../domain/entities';
 
 export class DishController {
 
   constructor(
     private dishRepositoryImpl: DishRepository,
+    private getSide: GetSide
   ) {}
 
   private handleError(error: unknown, res: Response) {
@@ -19,6 +22,7 @@ export class DishController {
   }
 
   public postDish = async (req: Request, res: Response) => {
+    const user = req.body.user as User;
     const [error, dishDto] = CreateDishDto.create(req.body);
 
     if (error) {
@@ -26,8 +30,8 @@ export class DishController {
       return;
     }
     
-    new CreateDish(this.dishRepositoryImpl)
-    .execute(dishDto!)
+    new CreateDish(this.dishRepositoryImpl, this.getSide)
+    .execute(dishDto!, user)
     .then( user => res.status(200).json(user))
     .catch( error => this.handleError(error, res));
   }
