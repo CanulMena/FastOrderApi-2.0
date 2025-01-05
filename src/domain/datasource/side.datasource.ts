e337d1eaa1e85420d801d92d13797982247ecae4
Gustavo Canul <gusssy12@gmail.com>
2025-01-04 20:48:18 -0600

    Add pagination Dto in the domain getSides from side repository and datasource

--- src/domain/datasource/side.datasource.ts
+++ src/domain/datasource/side.datasource.ts
@@ -1,10 +1,10 @@
-import { CreateSideDto, UpdateSideDto } from "../dtos/side/index";
+import { CreateSideDto, UpdateSideDto, PaginationDto } from "../dtos/index";
 import { Side } from "../entities/index";
 
 
 export abstract class  SideDatasource {
     abstract createSide( side: CreateSideDto ) : Promise<Side>;
-    abstract getSides() : Promise<Side[]>;
+    abstract getSides( pagination: PaginationDto ) : Promise<Side[]>;
     abstract getSideById( sideId: number ) : Promise<Side>;
     abstract deleteSide( sideId: number ) : Promise<Side>;
     abstract updateSide( side: UpdateSideDto ) : Promise<Side>;
--- src/domain/repositories/side.repository.ts
+++ src/domain/repositories/side.repository.ts
@@ -1,9 +1,9 @@
-import { CreateSideDto, UpdateSideDto } from "../dtos/side";
+import { CreateSideDto, UpdateSideDto, PaginationDto} from "../dtos/index";
 import { Side } from "../entities";
 
 export abstract class SideRepository {
     abstract createSide( side : CreateSideDto ) : Promise<Side>;
-    abstract getSides() : Promise<Side[]>;
+    abstract getSides( pagination: PaginationDto ) : Promise<Side[]>;
     abstract getSideById( sideId: number ) : Promise<Side>;
     abstract deleteBySide( sideId: number ) : Promise<Side>;
     abstract updateSide( side: UpdateSideDto ) : Promise<Side>;
