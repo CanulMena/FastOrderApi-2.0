c9eca12ea68711e342afba32b3eb41ce4a9657f3
Gustavo Canul <gusssy12@gmail.com>
2025-01-05 23:47:23 -0600

    add getSidesCount, getSidesByKitchenId, getSidesByKitchenIdCount in the domain side.datasource and side.repository

--- src/domain/datasource/side.datasource.ts
+++ src/domain/datasource/side.datasource.ts
@@ -5,6 +5,9 @@ import { Side } from "../entities/index";
 export abstract class  SideDatasource {
     abstract createSide( side: CreateSideDto ) : Promise<Side>;
     abstract getSides( pagination: PaginationDto ) : Promise<Side[]>;
+    abstract getSidesCount() : Promise<number>;
+    abstract getSidesByKitchenId( kitchenId: number, pagination: PaginationDto ) : Promise<Side[]>;
+    abstract getSidesByKitchenIdCount( kitchenId: number ) : Promise<number>;
     abstract getSideById( sideId: number ) : Promise<Side>;
     abstract deleteSide( sideId: number ) : Promise<Side>;
     abstract updateSide( side: UpdateSideDto ) : Promise<Side>;
--- src/domain/repositories/side.repository.ts
+++ src/domain/repositories/side.repository.ts
@@ -4,6 +4,9 @@ import { Side } from "../entities";
 export abstract class SideRepository {
     abstract createSide( side : CreateSideDto ) : Promise<Side>;
     abstract getSides( pagination: PaginationDto ) : Promise<Side[]>;
+    abstract getSidesCount() : Promise<number>;
+    abstract getSidesByKitchenId( kitchenId: number, pagination: PaginationDto ) : Promise<Side[]>;
+    abstract getSidesByKitchenIdCount( kitchenId: number ) : Promise<number>;
     abstract getSideById( sideId: number ) : Promise<Side>;
     abstract deleteBySide( sideId: number ) : Promise<Side>;
     abstract updateSide( side: UpdateSideDto ) : Promise<Side>;
