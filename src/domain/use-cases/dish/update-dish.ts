de5fb187ee22664052b4c1b5a068de0f8992c426
Gustavo Canul <gusssy12@gmail.com>
2025-01-09 12:40:48 -0600

    refactor updateDish use case

--- src/domain/use-cases/dish/update-dish.ts
+++ src/domain/use-cases/dish/update-dish.ts
@@ -15,47 +15,47 @@ export class UpdateDish implements UpdateDishUseCase {
   ) {}
 
   async execute(updateDishDto: UpdateDishDto, user: User): Promise<object> {
-    
-    // Valida que el platillo exista en la base de datos
+    // Verifica si el platillo existe
     const dishFound = await this.dishRepository.getDishById(updateDishDto.dishId);
-    // Si el platillo existe pero no pertenece a la misma cocina del usuario, y el usuario no es SUPER_ADMIN, lanza un error.
-    if(dishFound.kitchenId !== user.kitchenId && user.rol !== 'SUPER_ADMIN') {
+
+    // Verifica si el usuario tiene acceso a la cocina del platillo
+    if (dishFound.kitchenId !== user.kitchenId && user.rol !== 'SUPER_ADMIN') {
       throw CustomError.unAurothorized('User does not have access to this kitchen');
     }
 
-    // aca muere si no mando sidesId  r: como puedo solucinar esto? r: con un if que verifique si sidesId es undefined
-    if(updateDishDto.sidesId){
-
-      const updateDishDtosides = await Promise.all( //obtiene todos los sides del updateDto y verifica que existan en la base de datos.
-        updateDishDto.sidesId!.map(async (sideId) => {
-            const side = await this.sideRepository
-            .getSideById(sideId)
-            return side;
-        })
-      );
-  
-      const invalidSides = updateDishDtosides.filter( (side) => side.kitchenId !== dishFound.kitchenId); // filtra los sides que no pertenecen a la misma cocina del platillo a actualizar
-  
-      if (invalidSides.length > 0) { //si existe un complemento(side) que no pertenece a la misma cocina retorna un error con los sides que no pertenecen a la misma cocina
-        const invalidSideIds = invalidSides.map((side) => side.sideId).join(', ');
-        throw CustomError.unAurothorized(
-          `The following ${invalidSideIds.length > 1 ? 'sides' : 'side'} do not belong to the same kitchen: ${invalidSideIds}`
-        );
-      }
-      
+    // Valida los complementos (sides) si están presentes
+    if (updateDishDto.sidesId?.length) {
+      await this.validateAndUpdateSides(updateDishDto.sidesId, dishFound.kitchenId, updateDishDto.dishId);
     }
 
-    if( updateDishDto.sidesId && updateDishDto.sidesId.length > 0) {
-      //elimina todos los complementos del platillo
-      await this.dishSideRepository.deleteSidesByDishId(updateDishDto.dishId);
-      //crea los complementos del platillo a actualizar
-      await this.dishSideRepository.createMany(updateDishDto.dishId, updateDishDto.sidesId!);
-    }
-
+    // Actualiza el platillo
     const dishUpdated = await this.dishRepository.updateDish(updateDishDto);
 
-    return {
-      dishUpdated
-    };
+    return { dishUpdated };
+  }
+
+  private async validateAndUpdateSides(sidesId: number[], kitchenId: number, dishId: number): Promise<void> {
+    // Obtiene los complementos de la base de datos y valida que existan
+    const foundSides = await Promise.all(
+      sidesId.map(async (sideId) => {
+        const side = await this.sideRepository
+        .getSideById(sideId);
+        return side;
+      })
+    );
+
+    // Filtra los complementos que no pertenecen a la misma cocina
+    const invalidSides = foundSides.filter((side) => side.kitchenId !== kitchenId);
+
+    if (invalidSides.length) {
+      const invalidSideIds = invalidSides.map((side) => side.sideId).join(', ');
+      throw CustomError.unAurothorized(
+        `The following ${invalidSides.length > 1 ? 'sides' : 'side'} do not belong to the same kitchen: ${invalidSideIds}`
+      );
+    }
+
+    // Elimina y actualiza las relaciones con los complementos
+    await this.dishSideRepository.deleteSidesByDishId(dishId);
+    await this.dishSideRepository.createMany(dishId, sidesId);
   }
 }
