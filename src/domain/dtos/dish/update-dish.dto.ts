aee33cdd2bebb3f5658146f3409f00b89c9a0c31
Gustavo Canul <gusssy12@gmail.com>
2025-01-09 12:18:22 -0600

    add validations to updateDishDto

--- src/domain/dtos/dish/update-dish.dto.ts
+++ src/domain/dtos/dish/update-dish.dto.ts
@@ -9,16 +9,16 @@ export class UpdateDishDto {
     public readonly imagePath?: string,
   ){}
 
-  static create( object: {[key: string]: any} ): [string, UpdateDishDto?] {
+  static create( object: {[key: string]: any} ): [string?, UpdateDishDto?] {
     const { dishId, name, pricePerServing, pricePerHalfServing, sidesId, imagePath } = object;
-    if ( !dishId || isNaN( Number(dishId) ) ) return ['ID argument must be a valid number', undefined];
-    if ( name && typeof name !== 'string' ) return ['name must be a string', undefined];
-    if ( pricePerServing && typeof pricePerServing !== 'number' ) return ['pricePerServing must be a number', undefined];
-    if ( pricePerHalfServing && typeof pricePerHalfServing !== 'number' ) return ['pricePerHalfServing must be a number', undefined];
-    if ( sidesId && !Array.isArray(sidesId) ) return ['sidesId must be an array', undefined];
-    if ( sidesId && sidesId.some( (sideId: number) => typeof sideId !== 'number' ) ) return ['sidesId must be an array of numbers', undefined];
-    if ( imagePath && typeof imagePath !== 'string' ) return ['imagePath must be a string', undefined];
+    if ( !dishId || isNaN( Number(dishId) ) ) return ['ID argument must be a valid number'];
+    if ( name && typeof name !== 'string' ) return ['name must be a string'];
+    if ( pricePerServing && typeof pricePerServing !== 'number' ) return ['pricePerServing must be a number'];
+    if ( pricePerHalfServing && typeof pricePerHalfServing !== 'number' ) return ['pricePerHalfServing must be a number'];
+    if ( sidesId && !Array.isArray(sidesId) ) return ['sidesId must be an array'];
+    if ( sidesId && sidesId.some( (sideId: number) => typeof sideId !== 'number' ) ) return ['sidesId must be an array of numbers'];
+    if ( imagePath && typeof imagePath !== 'string' ) return ['imagePath must be a string'];
 
-    return [ '', new UpdateDishDto(dishId, name, pricePerServing, pricePerHalfServing, sidesId, imagePath) ];
+    return [ undefined, new UpdateDishDto(dishId, name, pricePerServing, pricePerHalfServing, sidesId, imagePath) ];
   }
 }
