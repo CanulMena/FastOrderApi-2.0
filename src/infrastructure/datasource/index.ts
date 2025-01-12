43317f6b0043fe5580c692ec7a9b749c835c132b
Gustavo Canul <gusssy12@gmail.com>
2025-01-12 16:02:14 -0600

    add order datasourceImpl in the infrastructure index

--- src/infrastructure/datasource/index.ts
+++ src/infrastructure/datasource/index.ts
@@ -4,3 +4,4 @@ export * from "./postgres-customer.datasource.impl";
 export * from "./postgres-side.datasource.impl";
 export * from "./postgres-dish.datasource.impl";
 export * from "./postgres-dish-side.datasource.impl";
+export * from "./postgres-order.datasource.impl";
