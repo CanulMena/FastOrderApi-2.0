2a4a9be31481b95718181dfc5e80a71f63319205
Gustavo Canul <gusssy12@gmail.com>
2025-01-13 00:32:00 -0600

    add register order in the orderRouest

--- src/presentation/order/order-routes.ts
+++ src/presentation/order/order-routes.ts
@@ -1,11 +1,19 @@
 import { Router } from "express";
+import { OrderController } from "./order-controller";
+import { PostgresOrderDatasourceImpl } from "../../infrastructure/datasource";
+import { OrderRepositoryImpl } from "../../infrastructure/repository";
 
 export class OrderRoutes {
   public static get routes(): Router {
     const router = Router();
 
-    
+    const orderDatasource = new PostgresOrderDatasourceImpl();
+    const orderRepository = new OrderRepositoryImpl(orderDatasource);
 
+    const orderController = new OrderController(orderRepository);
+
+    router.post('/register', orderController.registerOrder);
+    
     return router;
   }
 }
