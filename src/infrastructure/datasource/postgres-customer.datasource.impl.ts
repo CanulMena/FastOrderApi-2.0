271953b77f09ffa2bddd2cc26980d337d85c376e
Gustavo Canul <gusssy12@gmail.com>
2025-01-13 19:16:24 -0600

    add getCustomerById in the infrastructure layer customer.datasource.impl and customer.repository.impl

--- src/infrastructure/datasource/postgres-customer.datasource.impl.ts
+++ src/infrastructure/datasource/postgres-customer.datasource.impl.ts
@@ -2,13 +2,14 @@ import { PrismaClient } from "@prisma/client";
 import { CustomerDatasource } from "../../domain/datasource/customer.datasource";
 import { RegisterCustomerDto } from "../../domain/dtos/customer";
 import { Customer } from "../../domain/entities";
+import { CustomError } from "../../domain/errors";
 
 export class PostgresCustomerDatasourceImpl implements CustomerDatasource {
-
+  
   private readonly prisma = new PrismaClient().cliente;
-
+  
   async registerCustomer(registerCustomerDto: RegisterCustomerDto): Promise<Customer> {
-
+    
     const customerCreated = await this.prisma.create({
       data: {
         nombre: registerCustomerDto.name,
@@ -17,8 +18,22 @@ export class PostgresCustomerDatasourceImpl implements CustomerDatasource {
         cocinaId: registerCustomerDto.kitchenId,
       }
     });
-
+    
     return Customer.fromJson(customerCreated);
+    
+  }
+  
+  async getCustomerById(customerId: number): Promise<Customer> {
+
+    const customer = await this.prisma.findUnique({
+      where: {
+        id: customerId 
+        }
+    });
+
+    if(!customer) throw CustomError.notFound(`Customer with id ${customerId} not found`);
+
+    return Customer.fromJson(customer);
 
   }
 
