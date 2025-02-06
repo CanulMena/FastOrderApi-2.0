7016f0c7160099da582046c2fc3718d44470fca0
Gustavo Canul <gusssy12@gmail.com>
2025-02-06 13:08:04 -0600

    add getOrderDetailsByOrderId in domain datasource and repository

--- src/domain/repositories/order.repository.ts
+++ src/domain/repositories/order.repository.ts
@@ -3,7 +3,8 @@ import { Order, OrderDetail } from "../entities";
 
 export abstract class OrderRepository {
   abstract createOder(order: CreateOrderDto): Promise<Order>;
-  abstract updateOrder(order: UpdateOrderDto): Promise<Order>;
+  abstract updateOrder(order: UpdateOrderDto, orderDetailsEntity: OrderDetail[]): Promise<Order>;
   abstract getOrderById(orderId: number): Promise<Order>;
   abstract getOrderDetailById(orderDetailId: number): Promise<OrderDetail>;
+  abstract getOrderDetailsByOrderId(orderId: number): Promise<OrderDetail[]>
 }
