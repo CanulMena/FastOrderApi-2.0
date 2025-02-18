86419741b63e49599513b2a096cc8683c1168c3b
Gustavo Canul <gusssy12@gmail.com>
2025-02-17 23:57:03 -0600

    add index

--- src/domain/datasource/index.ts
+++ src/domain/datasource/index.ts
@@ -4,3 +4,4 @@ export * from "./side.datasource"
 export * from "./dish.datasource";
 export * from "./dish-side.datasource";
 export * from "./order.datasource";
+export * from "./file-upload.datasource";
