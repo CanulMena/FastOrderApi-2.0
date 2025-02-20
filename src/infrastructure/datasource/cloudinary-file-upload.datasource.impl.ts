54582765e7116ee8d8bac7e1b5815d223dcb1b25
Gustavo Canul <gusssy12@gmail.com>
2025-02-20 03:25:17 -0600

    create fileUploadSingle

--- src/infrastructure/datasource/cloudinary-file-upload.datasource.impl.ts
+++ src/infrastructure/datasource/cloudinary-file-upload.datasource.impl.ts
@@ -1,14 +1,19 @@
-import { UploadedFile } from "express-fileupload";
+import { UploadedFile } from "express-fileupload"; //¿Es esto un dependencia oculta? r: No, es una dependencia explicita
 import { FileUploadDatasource } from "../../domain/datasource";
+import { cloudinaryAdapter, ICloudinaryAdapter } from '../../configuration/plugins/cloudinary.adapter';
 
 export class CloudinaryFileUploadDataSourceImpl implements FileUploadDatasource {
   
   constructor(
-    // private readonly cloudinary: Cloudinary = cloudinaryAdapter.v2
+    private readonly cloudinaryAdaptert = cloudinaryAdapter //TODO: refacotizar esto cloudinaryAdapter
   ){}
 
-  fileUploadSingle(folder?: string, fileName?: string, file?: UploadedFile): Promise<string> {
-    throw new Error("Method not implemented.");
+  async fileUploadSingle(folder: string, fileName: string, file: UploadedFile): Promise<string>{
+    //TODO: AGREGAR CONTROL DE ERRORES
+    const filePath = file.tempFilePath; //el path del archivo temporal que se subió.
+    const result = await this.cloudinaryAdaptert.upload(filePath, folder, fileName);
+    return result.url;
+
   }
   fileUploadMultiple(): void {
     throw new Error("Method not implemented.");
