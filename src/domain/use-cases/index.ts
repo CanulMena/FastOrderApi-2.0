8caa8f619a62d07fc1a018d7ffd2e13cce3b6943
Gustavo Canul <gusssy12@gmail.com>
2025-02-20 15:19:09 -0600

    refactor importations from order-controller

--- src/domain/use-cases/index.ts
+++ src/domain/use-cases/index.ts
@@ -3,5 +3,6 @@ export * from './customer/index';
 export * from './dish/index';
 export * from './kitchen/index';
 export * from './side/index';
-export * from './order/index';
 export * from './file-upload/index';
+export * from './order/index';
+export * from './order-detail/index';
---
+++ src/domain/use-cases/order-detail/index.ts
@@ -0,0 +1,2 @@
+export * from './create-order-detail';
+export * from './delete-order-detail';
