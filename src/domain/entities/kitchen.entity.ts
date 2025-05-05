ab23f0f2c08e60e82603aada48e7a56f495f6919
Angel Yahir Poot Ucan <135137591+YahirPoot@users.noreply.github.com>
2025-05-04 22:00:19 -0600

    Add the day on which the dishes are scheduled and in the kitchen's get by ID response, modify it so that you can obtain the number of dishes, complements, clients and users that are related to that kitchen.

--- src/domain/entities/kitchen.entity.ts
+++ src/domain/entities/kitchen.entity.ts
@@ -6,17 +6,31 @@ export class Kitchen {
         public name: string, //Nombre de la cocina
         public address: string, //Dirección para ubicar a la cocina
         public phone: string, //Numero para contactar a la la cocina
+        public dishCount?: number, 
+        public sideCount?: number,
+        public customerCount?: number, 
+        public userCount?: number
     ) {}
     
     static fromJson = ( object: {[key: string] : any} ) : Kitchen => {
-        const { id, nombre, direccion, telefono } = object;
+        const { id, nombre, direccion, telefono, _count } = object;
 
         if ( !id ) throw CustomError.badRequest('id is required');
         if ( !nombre ) throw CustomError.badRequest('nombre is required');
         if ( !direccion ) throw CustomError.badRequest('direccion is required');
         if ( !telefono ) throw CustomError.badRequest('telefono is required');
 
-        return new Kitchen(id, nombre, direccion, telefono);
+
+        return new Kitchen(
+            id, 
+            nombre, 
+            direccion, 
+            telefono,
+            _count?.platillos ?? 0,
+            _count?.complementos ?? 0, 
+            _count?.clientes ?? 0, 
+            _count?.usuarios ?? 0
+        );
     }
 
 }
