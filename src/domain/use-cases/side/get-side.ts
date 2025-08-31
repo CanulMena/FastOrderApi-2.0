97a2a945c57157e80fb873d7ed97ed602b288b05
AngelPoot <angelpootucan7@gmail.com>
2025-08-31 15:55:24 -0600

    feat(domain): Include related dishes when getting a side

--- src/domain/use-cases/side/get-side.ts
+++ src/domain/use-cases/side/get-side.ts
@@ -1,13 +1,16 @@
 import { Side, User } from "../../entities";
 import { CustomError } from "../../errors";
-import { SideRepository } from "../../repositories";
+import { DishRepository, SideRepository } from "../../repositories";
 
 interface GetSideUseCase {
   execute(sideId: number, user: User): Promise<object>;
 }
 
 export class GetSide implements GetSideUseCase {
-  constructor(private readonly sideRepository: SideRepository) {}
+  constructor(
+    private readonly sideRepository: SideRepository,
+    private readonly dishRepository: DishRepository,
+  ) {}
 
   async execute(sideId: number, user: User): Promise<object> {
     if (!user.rol) throw CustomError.unAuthorized('User role is required');
@@ -15,8 +18,10 @@ export class GetSide implements GetSideUseCase {
     if(side.kitchenId !== user.kitchenId && user.rol !== 'SUPER_ADMIN') {
       throw CustomError.unAuthorized('User does not have access to this kitchen');
     }
+    const dishes = await this.dishRepository.getDishesBySideId(sideId);
     return {
-      side: side
+      ...side, 
+      dishes
     }
   }
 }
