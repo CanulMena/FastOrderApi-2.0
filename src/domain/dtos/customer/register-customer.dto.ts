fa4f08a225f699a11156d20beb851e8db7685436
Gustavo Mena <gustavocanulmena@gmail.com>
2025-10-29 08:48:14 -0600

    comment phone number and adress in register-customer.dto

--- src/domain/dtos/customer/register-customer.dto.ts
+++ src/domain/dtos/customer/register-customer.dto.ts
@@ -1,9 +1,9 @@
 
 export class RegisterCustomerDto {
   constructor(
-    public name: string,   
+    public name: string,
     public kitchenId: number,
-    public phone?: string,    
+    public phone?: string,
     public address?: string,
   ) {}
 
@@ -11,9 +11,9 @@ export class RegisterCustomerDto {
     const { name, kitchenId, phone, address } = props;
     if (!name) return ['Missing Name'];
     if (!kitchenId) return ['Missing Kitchen ID'];
-    if (phone !== null && typeof phone !== 'string') return ['Phone must be a string or null'];
-    if (phone && phone.length !== 10) return ['Invalid Phone'];
-    if (address !== null && typeof address !== 'string') return ['Address must be a string or null'];
+    // if (phone !== null && typeof phone !== 'string') return ['Phone must be a string or null'];
+    // if (phone && phone.length !== 10) return ['Invalid Phone'];
+    // if (address !== null && typeof address !== 'string') return ['Address must be a string or null'];
     if (kitchenId !== null && (typeof kitchenId !== 'number' || kitchenId <= 0)) return ['Kitchen ID must be a positive number or null'];
     //TODO: Investigar y validar el formato de un número de teléfono y dirección -> regular expressions
 
